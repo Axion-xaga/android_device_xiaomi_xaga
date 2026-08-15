b7c1f6475f7432bb52388861cc520abeab0dd822
Jefino9488 <jefinojacob9488@gmail.com>
2026-08-15 14:24:22 +0530

    xaga: add Bypass lock state

--- lineage_xaga.mk
+++ lineage_xaga.mk
@@ -14,6 +14,8 @@ $(call inherit-product, device/xiaomi/xaga/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+$(call soong_config_set,fastbootd,bypass_lock_state,true)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2460
 TARGET_SCREEN_WIDTH := 1080
